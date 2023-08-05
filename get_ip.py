dcc05fdadf48e8e35a3d0effc8207a0977fe681f
sfog17 <40466555+sfog17@users.noreply.github.com>
2023-08-05 11:05:57 +0100

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -1,7 +1,5 @@
 def get_ip():
     """Find IP address of the server"""
-    wifi_name = get_wifi_name()
-    print(wifi_name)
     current_datetime = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     f_name=f'{current_datetime}_ip_info.json'
     r = requests.get('https://ipinfo.io/')
