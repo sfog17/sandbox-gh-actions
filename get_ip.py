831a8735f666186716b684392f9f0e11287a2ccd
sfog17 <40466555+sfog17@users.noreply.github.com>
2023-08-06 13:23:18 +0100

    Output to folder

--- get_ip.py
+++ get_ip.py
@@ -1,13 +1,16 @@
 import datetime
 import requests
+from pathlib import Path
 
 def get_ip():
     """Find IP address of the server"""
     current_datetime = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-    f_name=f'{current_datetime}_ip_info.json'
+    output_dir = Path(__file__).parent / 'output'
+    output_dir.mkdir(parents=True, exist_ok=True)
+    output_file= output_dir / f'{current_datetime}_ip_info.json'
     r = requests.get('https://ipinfo.io/')
     print(r.text)
-    with open(f_name, 'w') as f_out:
+    with open(output_file, 'w') as f_out:
         f_out.write(r.text)
 
 
